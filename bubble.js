bf07224a9bb3d580acad08d0bda1aefe97ff793d
ayusht22 <ayushtiwaryy22@gmail.com>
2022-04-15 17:35:52 +0530

    more timesteps

--- bubble.js
+++ bubble.js
@@ -1,32 +1,51 @@
-first_stress=[]
-second_stress=[]
+
+all_stress=[[],[],[],[],[]]
+
 d3.csv('stress.csv',function(data){
 
     for (var i = 0; i < data.length; i++) {
-        first_stress.push(data[i]['NS-Initial'])
-        second_stress.push(data[i]['NS-Final'])
-        
+        all_stress[0].push(data[i]['ts0'])
+        all_stress[1].push(data[i]['ts200'])
+        all_stress[2].push(data[i]['ts500'])
+        all_stress[3].push(data[i]['ts700'])
+        all_stress[4].push(data[i]['ts800'])
       }
     
 
 } ) 
 
 
-first_coordinates={}
-first_coordinates['x']=[]
-first_coordinates['y']=[]
-second_coordinates={}
-second_coordinates['x']=[]
-second_coordinates['y']=[]
+ts0_coordinates={}
+ts0_coordinates['x']=[]
+ts0_coordinates['y']=[]
+
+ts200_coordinates={}
+ts200_coordinates['x']=[]
+ts200_coordinates['y']=[]
+
+ts500_coordinates={}
+ts500_coordinates['x']=[]
+ts500_coordinates['y']=[]
+
+ts700_coordinates={}
+ts700_coordinates['x']=[]
+ts700_coordinates['y']=[]
+
+ts800_coordinates={}
+ts800_coordinates['x']=[]
+ts800_coordinates['y']=[]
 states=[]
-states1=[]
+states2=[]
+states3=[]
+states4=[]
+states5=[]
 progressLine=[]
 
 barPlot={}
 
 
 
-d3.json('file1.json',function(data){
+d3.json('sdg.json',function(data){
 
     for (var i = 0; i < data.length; i++) {
 
@@ -37,18 +56,38 @@ d3.json('file1.json',function(data){
         for(let k=0;k<state.length-1;k++){
           stateName+=(state[k]+" ")
         }
-        if(states.length<=35){states.push(stateName)}
-        else states1.push(stateName+" 1")
-      
+       
+         
+       
+        
+        
+       
         if(state[state.length-1]=='firstTimestamp'){
-        first_coordinates['x'].push(data[i]['ld'][0])
-        first_coordinates['y'].push(data[i]['ld'][1])
+        ts0_coordinates['x'].push(data[i]['ld'][0])
+        ts0_coordinates['y'].push(data[i]['ld'][1])
+        states.push(stateName)
         }
-
-        if(state[state.length-1]=='secondTimestamp'){
-        second_coordinates['x'].push(data[i]['ld'][0])
-        second_coordinates['y'].push(data[i]['ld'][1])
+        else if(state[state.length-1]=='secondTimestamp'){
+        ts200_coordinates['x'].push(data[i]['ld'][0])
+        ts200_coordinates['y'].push(data[i]['ld'][1])
+        states2.push(stateName+" 2")
         }
+        else if(state[state.length-1]=='thirdTimestamp'){
+          ts500_coordinates['x'].push(data[i]['ld'][0])
+          ts500_coordinates['y'].push(data[i]['ld'][1])
+          states3.push(stateName+" 3")
+          }
+        else if(state[state.length-1]=='fourthTimestamp'){
+          ts700_coordinates['x'].push(data[i]['ld'][0])
+          ts700_coordinates['y'].push(data[i]['ld'][1])
+          states4.push(stateName+" 4")
+          }
+        else if(state[state.length-1]=='fifthTimestamp'){
+          ts800_coordinates['x'].push(data[i]['ld'][0])
+          ts800_coordinates['y'].push(data[i]['ld'][1])
+          states5.push(stateName+" 5")
+          }
+        
         let aspC=0
         let frontC=0
         let achieverC =0
@@ -71,7 +110,8 @@ d3.json('file1.json',function(data){
           }
         }
         
-        if(state[state.length-1]=='firstTimestamp'){
+        if(state[state.length-1]=='firstTimestamp')
+        {
         barPlot[stateName]=
           {
             x: ['Aspirants', 'Performers', 'Front Runners','Achievers'],
@@ -83,30 +123,76 @@ d3.json('file1.json',function(data){
             
           }
         }
-        else{
+        else if(state[state.length-1]=='secondTimestamp')
+        {
          
-          let stt=stateName+" 1"
-          barPlot[stt]=
-          {
-            x: ['Aspirants', 'Performers', 'Front Runners','Achievers'],
-            y: [aspC, performerC,frontC,achieverC],
-            type: 'bar',
-            marker:{
-              color:['#ea9393','#ffbf86','#95cf95','#87ceeb']
-            },
-            
-          }
+            let stt=stateName+" 2"
+            barPlot[stt]=
+            {
+              x: ['Aspirants', 'Performers', 'Front Runners','Achievers'],
+              y: [aspC, performerC,frontC,achieverC],
+              type: 'bar',
+              marker:{
+                color:['#ea9393','#ffbf86','#95cf95','#87ceeb']
+              },
+              
+            }
+        }
+        else if(state[state.length-1]=='thirdTimestamp')
+        {
+         
+            let stt=stateName+" 3"
+            barPlot[stt]=
+            {
+              x: ['Aspirants', 'Performers', 'Front Runners','Achievers'],
+              y: [aspC, performerC,frontC,achieverC],
+              type: 'bar',
+              marker:{
+                color:['#ea9393','#ffbf86','#95cf95','#87ceeb']
+              },
+              
+            }
+        }
+        else if(state[state.length-1]=='fourthTimestamp')
+        {
+         
+            let stt=stateName+" 4"
+            barPlot[stt]=
+            {
+              x: ['Aspirants', 'Performers', 'Front Runners','Achievers'],
+              y: [aspC, performerC,frontC,achieverC],
+              type: 'bar',
+              marker:{
+                color:['#ea9393','#ffbf86','#95cf95','#87ceeb']
+              },
+              
+            }
+        }
+        else if(state[state.length-1]=='fifthTimestamp')
+        {
+         
+            let stt=stateName+" 5"
+            barPlot[stt]=
+            {
+              x: ['Aspirants', 'Performers', 'Front Runners','Achievers'],
+              y: [aspC, performerC,frontC,achieverC],
+              type: 'bar',
+              marker:{
+                color:['#ea9393','#ffbf86','#95cf95','#87ceeb']
+              },
+              
+            }
         }
 
       }
 
-
+/*                                                            progress line                                         */
       for(let i =0;i<36;i++){
         
         progressLine.push( {
        
-          x: first_coordinates['x'][i],
-          y: first_coordinates['y'][i],
+          x: ts0_coordinates['x'][i],
+          y: ts0_coordinates['y'][i],
           mode: 'lines+markers',
            line: {simplify: false},
           name:states[i],
@@ -118,8 +204,58 @@ d3.json('file1.json',function(data){
       for(let i=0;i<36;i++){
         progressLine.push( {
        
-          x: [first_coordinates['x'][i],second_coordinates['x'][i]],
-          y: [first_coordinates['y'][i],second_coordinates['y'][i]],
+          x: [ts0_coordinates['x'][i],ts200_coordinates['x'][i]],
+          y: [ts0_coordinates['y'][i],ts200_coordinates['y'][i]],
+          mode: 'lines+markers',
+          name:states[i],
+          visible:'legendonly',
+          line:{
+            shape:'spline',
+            dash:"dot"
+          }
+         
+        })
+      }
+
+      for(let i=0;i<36;i++){
+        progressLine.push( {
+       
+          x: [ts0_coordinates['x'][i],ts200_coordinates['x'][i],ts500_coordinates['x'][i]],
+          y: [ts0_coordinates['y'][i],ts200_coordinates['y'][i],ts500_coordinates['y'][i]],
+          mode: 'lines+markers',
+          name:states[i],
+          visible:'legendonly',
+          line:{
+            shape:'spline',
+            dash:"dot"
+          }
+         
+        })
+      }
+
+
+      for(let i=0;i<36;i++){
+        progressLine.push( {
+       
+          x: [ts0_coordinates['x'][i],ts200_coordinates['x'][i],ts500_coordinates['x'][i],ts700_coordinates['x'][i]],
+          y: [ts0_coordinates['y'][i],ts200_coordinates['y'][i],ts500_coordinates['y'][i],ts700_coordinates['y'][i]],
+          mode: 'lines+markers',
+          name:states[i],
+          visible:'legendonly',
+          line:{
+            shape:'spline',
+            dash:"dot"
+          }
+         
+        })
+      }
+
+
+      for(let i=0;i<36;i++){
+        progressLine.push( {
+       
+          x: [ts0_coordinates['x'][i],ts200_coordinates['x'][i],ts500_coordinates['x'][i],ts700_coordinates['x'][i],ts800_coordinates['x'][i]],
+          y: [ts0_coordinates['y'][i],ts200_coordinates['y'][i],ts500_coordinates['y'][i],ts700_coordinates['y'][i],ts800_coordinates['y'][i]],
           mode: 'lines+markers',
           name:states[i],
           visible:'legendonly',
@@ -135,7 +271,7 @@ d3.json('file1.json',function(data){
      // console.log(data)
 
 
-      years=['1','2']
+      years=['1','2','3','4','5']
       var sliderSteps = [];
       for (i = 0; i < years.length; i++) {
         sliderSteps.push({
@@ -143,26 +279,30 @@ d3.json('file1.json',function(data){
           label: years[i],
           args: [[years[i]], {
             mode: 'immediate',
-            transition: {duration: 300},
-            frame: {duration: 300, redraw: false},
+            transition: {duration: 1000},
+            frame: {duration: 1000, redraw: false},
           }]
         });
       }
+
+
+/*                                                         frames at each time step                                                             */
+
 
       var frames = [];
       
         frames.push({
           name: years[0],
           data: [{
-            x: first_coordinates['x'],
-            y: first_coordinates['y'],
+            x: ts0_coordinates['x'],
+            y: ts0_coordinates['y'],
             id: states,
             text: states,
-            marker: {size: first_stress,
+            marker: {size: all_stress[0],
               sizemode: 'area',
           
             },
-            sizeref:Math.max(first_stress)/Math.max(first_stress)**2,
+            sizeref:Math.max(all_stress[0])/Math.max(all_stress[0])**2,
             
           }
         ]}
@@ -172,23 +312,92 @@ d3.json('file1.json',function(data){
         frames.push({
           name: years[1],
           data:[ {
-            x: second_coordinates['x'],
-            y: second_coordinates['y'],
-            id: states,
-            text: states1,
+            x: ts200_coordinates['x'],
+            y: ts200_coordinates['y'],
+            id: states,  
+            text: states2,
             marker: {
-              size: second_stress,
+              size: all_stress[1],
               sizemode: 'area',
           
             },
-            sizeref:Math.max(second_stress)/Math.max(second_stress)**2,
+            sizeref:Math.max(all_stress[1])/Math.max(all_stress[1])**2,
       
           },
          
         ]
         })
 
-        for(let i=36;i<progressLine.length;i++)frames[1].data.push(progressLine[i])
+        for(let i=36;i<72;i++)frames[1].data.push(progressLine[i])
+
+
+        frames.push({
+          name: years[2],
+          data:[ {
+            x: ts500_coordinates['x'],
+            y: ts500_coordinates['y'],
+            id: states,
+            text: states3,
+            marker: {
+              size: all_stress[2],
+              sizemode: 'area',
+          
+            },
+            sizeref:Math.max(all_stress[2])/Math.max(all_stress[2])**2,
+      
+          },
+         
+        ]
+        })
+
+        for(let i=72;i<108;i++)frames[2].data.push(progressLine[i])
+
+
+
+        frames.push({
+          name: years[3],
+          data:[ {
+            x: ts700_coordinates['x'],
+            y: ts700_coordinates['y'],
+            id: states,
+            text: states4,
+            marker: {
+              size: all_stress[3],
+              sizemode: 'area',
+          
+            },
+            sizeref:Math.max(all_stress[3])/Math.max(all_stress[3])**2,
+      
+          },
+         
+        ]
+        })
+
+        for(let i=108;i<144;i++)frames[3].data.push(progressLine[i])
+
+
+       frames.push({
+          name: years[4],
+          data:[ {
+            x: ts800_coordinates['x'],
+            y: ts800_coordinates['y'],
+            id: states,
+            text: states5,
+            marker: {
+              size: all_stress[4],
+              sizemode: 'area',
+          
+            },
+            sizeref:Math.max(all_stress[4])/Math.max(all_stress[4])**2,
+      
+          },
+         
+        ]
+        })
+
+        for(let i=144;i<180;i++)frames[4].data.push(progressLine[i])
+
+
       
         console.log(progressLine.length)
         console.log(frames[0].data)
@@ -205,7 +414,16 @@ d3.json('file1.json',function(data){
         },
         
         hovermode: 'closest',
-        autosize:true,
+       // autosize:true,
+       margin: {
+      //  l: 50,
+        r: 150,
+       // b: 100,
+       // t: 100,
+        pad: 4
+      },
+        showlegend:true,
+        
        // We'll use updatemenus (whose functionality includes menus as
        // well as buttons) to create a play button and a pause button.
        // The play button works by passing `null`, which indicates that
@@ -228,7 +446,7 @@ d3.json('file1.json',function(data){
               mode: 'immediate',
               fromcurrent: true,
               transition: {duration: 1000},
-              frame: {duration: 500, redraw: true}
+              frame: {duration: 1000, redraw: true}
             }],
             label: 'Play'
           }, {
@@ -259,16 +477,16 @@ d3.json('file1.json',function(data){
 
     let dat= [{
        
-      x: first_coordinates['x'],
-      y: [],
+      x: ts0_coordinates['x'],
+      y: ts0_coordinates['y'],
       mode: 'markers',
       id: states,
       text: states,
-     
+      
       marker: {
-        size: first_stress,
+        size: all_stress[0],
         sizemode: 'area',
-        sizeref:Math.max(first_stress)/Math.max(first_stress)**2
+        sizeref:Math.max(all_stress[0])/Math.max(all_stress[0])**2
       }
     }
 
@@ -280,11 +498,12 @@ d3.json('file1.json',function(data){
     dat.push({
        
       x: progressLine[i]['x'],
-      y: [],
+      y: progressLine[i]['y'],
       mode: 'lines+markers',
       line:{
         shape:'spline'
-      }
+      },
+     
     })
   }
       Plotly.newPlot('myDiv', {
@@ -299,19 +518,19 @@ console.log(barPlot)
 
 const modal = document.querySelector(".modal");
 
-const closeButton = document.querySelector(".close-button");
+//const closeButton = document.querySelector(".close-button");
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+  modal.style.display = "block";
 }
 
 function windowOnClick(event) {
     if (event.target === modal) {
-        toggleModal();
+      modal.style.display = "none";
     }
 }
 
-closeButton.addEventListener("click", toggleModal);
+//closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
 
@@ -320,14 +539,13 @@ console.log(barPlot)
 
 var myPlot = document.getElementById('myDiv')
 myPlot.on('plotly_click', function(data){
-  console.log(data)
+console.log(data)
   
  // c(barPlot[data['points'][0]['text']])
- toggleModal()
+toggleModal()
+d=document.getElementById('br')
 
-console.log(barPlot[data['points'][0]['text']])
-
-  Plotly.newPlot('bar', {
+ Plotly.react('bar', {
     data:[barPlot[data['points'][0]['text']]],
     layout:{title:{text:data['points'][0]['text']}}
  
